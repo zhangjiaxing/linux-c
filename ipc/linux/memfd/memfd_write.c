216b9ec082e1dac0147a282551053be014abd95b
zhiliao <zhang.jiaxing@foxmail.com>
2022-03-21 19:42:23 +0800

    add memfd

---
+++ ipc/linux/memfd/memfd_write.c
@@ -0,0 +1,91 @@
+#define _GNU_SOURCE
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/memfd.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+
+#define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define K 1024
+#define M (K*1024)
+
+#define FILESIZE (2 * M)
+
+
+struct memtext {
+    sem_t sem_write;
+    sem_t sem_read;
+    char text[1];
+};
+
+int main(void){
+    int fd;
+    void *addr;
+
+    fd = memfd_create("memfd_test", MFD_CLOEXEC);
+    if (fd == -1){
+        errExit("memfd_create");
+    }
+
+    if (ftruncate(fd, FILESIZE) == -1){
+        errExit("truncate");
+    }
+
+    addr = mmap(NULL, FILESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    if(addr == MAP_FAILED){
+        errExit("mmap");
+    }
+
+    struct memtext *mtext = addr;
+    sem_init(&mtext->sem_write, 1, 0);
+    sem_init(&mtext->sem_read, 1, 1);
+
+    pid_t pid = fork();
+    if(pid > 0){
+        //parent
+        sem_wait(&mtext->sem_read);
+        sprintf(mtext->text, "C++\n");
+        sem_post(&mtext->sem_write);
+
+        sem_wait(&mtext->sem_read);
+        sprintf(mtext->text, "java\n");
+        sem_post(&mtext->sem_write);
+        
+        sem_wait(&mtext->sem_read);
+        sprintf(mtext->text, "python\n");
+        sem_post(&mtext->sem_write);
+
+        sem_wait(&mtext->sem_read);
+        sprintf(mtext->text, "go\n");
+        sem_post(&mtext->sem_write);
+
+        sleep(1);
+        sem_destroy(&mtext->sem_read);
+        sem_destroy(&mtext->sem_write);
+        exit(0);
+        
+    }else if(pid == 0){
+        //child
+        while(1){
+            sem_wait(&mtext->sem_write);
+            char *cur = mtext->text;
+            printf("> ");
+            while(*cur != '\0'){
+                putchar(toupper(*cur));
+                cur++;
+            }
+            sem_post(&mtext->sem_read);
+        }
+        
+    }else{
+        //error
+        errExit("fork");
+    }
+
+    return 0;
+}
+
